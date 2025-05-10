84fbcd3fcc44aa5748731f8b93dcf37a06722ec4
Vasuki028 <sejivasko@gmail.com>
2025-05-10 19:14:17 +0530

    initial commit

---
+++ src/Pokedex/Pokedex.jsx
@@ -0,0 +1,11 @@
+import Search from "../Components/Search/Search";
+import '../Components/Pokedex.css';
+function Pokedex() {
+return(
+<div className="Pokedex-wrapper">
+<h1 id = "heading"> Pokedex </h1>
+<Search/>
+</div>
+)
+}
+export default Pokedex;
